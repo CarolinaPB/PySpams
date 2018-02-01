1687bd68cf1def6b64c908ccc45629dfda494a43
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-02-01 21:24:37 +0000

    Writes info to a new file

--- carolina_0.03.py
+++ carolina_0.03.py
@@ -15,41 +15,20 @@ inideme_res=''
 for i in inideme_size:
 	inideme_res= inideme_res +i +" "
 print inideme_res
-###Initial migration matrix
 
-output_file.write("Sampling vector \n")
+
+
+output_file.write("# Number of loci \n")
+output_file.write(loci)
+output_file.write("\n")
+output_file.write("# Sampling vector \n")
 output_file.write(vector_res)
+output_file.write("\n")
+output_file.write("# Initial deme sizes\n")
+output_file.write(inideme_res)
 output_file.write("\n")
 
 
 
 output_file.close()
-
-
-
-#test= (loci, samp_vector)
-#print test
-
-#for i in test:
-	#print i
-
-#input_file.write(loci)
-#input_file.write(samp_vector)
-
-
-
-
-
-
-
-
-#loci = int(raw_input("# Number of loci \n"))
-
-#info_doc = ["raw_input1", "raw_input2"]
-#file.write(info_doc)
-
-#input_file = open("input_test.txt","w")
-#input_file.write(loci)
-
-#input_file.close()
 
