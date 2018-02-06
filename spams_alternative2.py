3a66a99383c9ea9b6de28b25584c0ff5ffe94994
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-02-06 18:16:12 +0000

    Opens window with buttons and fields to enter information

--- spams_alternative2.py
+++ spams_alternative2.py
@@ -1,3 +1,4 @@
+#To use this first you need to install and configure PyQt4
 from PyQt4 import QtCore, QtGui
 
 
@@ -13,20 +14,24 @@ class Dialog(QtGui.QDialog):
       
 
         buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel)
-
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
-
+      
+     
         mainLayout = QtGui.QVBoxLayout()
         
         mainLayout.addWidget(self.horizontalGroupBox)
         mainLayout.addWidget(self.gridGroupBox)
         mainLayout.addWidget(buttonBox)
+   
 	self.setGeometry(300, 300, 350, 300)
 
         self.setLayout(mainLayout)
         self.setWindowTitle("SPAms alternative")
-
+        
+        
+    def handleButton(self):
+        print("hello world")
 
     def createHorizontalGroupBox(self):
         self.horizontalGroupBox = QtGui.QGroupBox("Population dynamics")
@@ -42,38 +47,31 @@ class Dialog(QtGui.QDialog):
     def createGridGroupBox(self):
         self.gridGroupBox = QtGui.QGroupBox("Options")
         layout = QtGui.QGridLayout()
-        sections = ("Number of loci", "Sampling vector", "Initial deme sizes", "Initial migration matrix", "Time of change", "Deme sizes")
-	answers_input= ("ans1","ans2", "ans3", "ans4","ans5","ans6")
-	answers = ("answer1", "answer2", "answer3", "answer4", "answer5", "answer6")
-	quest = ("q1","q2", "q3", "q4", "q5","q6")
+        sections = ("Number of loci", "Sampling Vector", "Initial deme sizes", "Initial migration matrix", "Time of change", "Deme sizes")
 	
-  
+        output_file = open("alternative2_result.txt","w")
+    
+   
+        for n in range(len(sections)): 
+            layout.addWidget(QtGui.QLabel(sections[n]),n,0)
+            self.line = QtGui.QLineEdit()
+           
+            layout.addWidget(self.line, n,1)
+            output_file.write("# "+sections[n]+"\n")
+            output_file.write(self.line.text())
+            output_file.write("\n")
 
-	for n in range(len(sections)):
-	    
-	    layout.addWidget(QtGui.QLabel(sections[n]),n,0)
- 	    self.answers[n] = QtGui.QLabel()
-	    quest[n] = QtGui.QLineEdit()
-	    quest[n].textChanged.connect(self.answers_input[n]
+           
 
-	    layout.addWidget(quest[n], n, 1)
-
-
-
-     
         self.gridGroupBox.setLayout(layout)
 	
 	
-	
-    
-	   
-
 
 if __name__ == '__main__':
 
     import sys
-
     app = QtGui.QApplication(sys.argv)
     dialog = Dialog()
+  
     
 sys.exit(dialog.exec_())
