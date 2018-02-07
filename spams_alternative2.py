743d902187c27fc5ca785635eb925a31989373a3
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-02-07 13:28:16 +0000

    Clicking buttons opens empty window. More input fields

--- spams_alternative2.py
+++ spams_alternative2.py
@@ -1,11 +1,10 @@
 #To use this first you need to install and configure PyQt4
 from PyQt4 import QtCore, QtGui
+import sys
 
 
 class Dialog(QtGui.QDialog):
-    NumGridRows = 6
-    NumButtons = 4
-
+    
     def __init__(self):
         super(Dialog, self).__init__()
 
@@ -28,17 +27,20 @@ class Dialog(QtGui.QDialog):
 
         self.setLayout(mainLayout)
         self.setWindowTitle("SPAms alternative")
+  
         
-        
-    def handleButton(self):
-        print("hello world")
+    def new_window(self):
+        self.wind.show()
+  
 
     def createHorizontalGroupBox(self):
         self.horizontalGroupBox = QtGui.QGroupBox("Population dynamics")
         layout = QtGui.QHBoxLayout()
-        models = ("Stepping stone", "Island","model1", "model2")
+        models = ("Admixture model", "One population size change","Population structure")
         for m in models:
             button = QtGui.QPushButton(m)
+            button.clicked.connect(self.new_window)
+            self.wind = Second_W(self)
             layout.addWidget(button)
 
         self.horizontalGroupBox.setLayout(layout)
@@ -50,28 +52,43 @@ class Dialog(QtGui.QDialog):
         sections = ("Number of loci", "Sampling Vector", "Initial deme sizes", "Initial migration matrix", "Time of change", "Deme sizes")
 	
         output_file = open("alternative2_result.txt","w")
-    
    
         for n in range(len(sections)): 
             layout.addWidget(QtGui.QLabel(sections[n]),n,0)
             self.line = QtGui.QLineEdit()
-           
             layout.addWidget(self.line, n,1)
+            for i in range(2,5):
+                self.line2 = QtGui.QLineEdit()
+                self.line3 = QtGui.QLineEdit()
+                self.line4 = QtGui.QLineEdit()
+                layout.addWidget(self.line2, 1, i )
+                layout.addWidget(self.line3, 2, i )
+                layout.addWidget(self.line4, 5, i )
+
+
+            
             output_file.write("# "+sections[n]+"\n")
             output_file.write(self.line.text())
             output_file.write("\n")
 
            
-
         self.gridGroupBox.setLayout(layout)
-	
-	
 
-if __name__ == '__main__':
 
-    import sys
+class Second_W(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(Second_W, self).__init__(parent)
+        self.setGeometry(300, 300, 350, 300)
+       
+      	
+
+def main():
     app = QtGui.QApplication(sys.argv)
     dialog = Dialog()
-  
-    
-sys.exit(dialog.exec_())
+    dialog.show()
+    sys.exit(dialog.exec_())
+
+if __name__ == '__main__':
+    main()
+
+
