b6cef3a7a79375e91570b5bcfa1d08fa298ad689
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-03-02 18:12:19 +0000

    Stores input in array. Problems with saving to file

--- input_arrays.py
+++ input_arrays.py
@@ -1,8 +1,8 @@
 from PyQt4 import QtCore, QtGui
 import sys
 import numpy as np
-#from teste import save_file
-np.set_printoptions(suppress=True)
+
+
 
 class Dialog(QtGui.QDialog):
 
@@ -21,15 +21,13 @@ class Dialog(QtGui.QDialog):
 
         self.input_btn = QtGui.QPushButton("Save input")
         self.input_btn.clicked.connect(self.store_input)
+
         self.file_input = QtGui.QPushButton("Save doc")
         self.file_input.clicked.connect(self.save_files)
         mainLayout.addWidget(self.input_btn)
         mainLayout.addWidget(self.file_input)
         mainLayout.addWidget(buttonBox)
         self.setLayout(mainLayout)
-
-        
-        
 
     def createGridGroupBox(self):
         self.gridGroupBox = QtGui.QGroupBox("Options")
@@ -55,76 +53,83 @@ class Dialog(QtGui.QDialog):
         layout.addWidget(self.line5, 6,1)
   
         self.gridGroupBox.setLayout(layout)
-    
-    def store_input(self):
-        
-        #m_array = np.zeros((6, 1),dtype=object)
-        #narray0 = np.zeros((6,1),dtype=object)
-        
+    def run_once(func):
+        def decorated(*args, **kwargs):
+            try:
+                return decorated._once_result
+            except AttributeError:
+                decorated._once_result = func(*args, **kwargs)
+                return decorated._once_result
+        return decorated
 
+    @run_once
+    def create_arrays(self):
         init_array = np.zeros((6,10),dtype=object)
         sect_array = np.array([["Number of loci"],["Sampling vector"],["Initial deme sizes"],["Initial migration matrix"],["Time of change"],["Deme sizes"]])
         sect_array = np.vstack(sect_array)
         total_array = np.hstack((sect_array,init_array))
-        #print total_array
+        return total_array
+    
 
+    def store_input(self):
         numinput0 = self.line0.text()
         numinput1 = self.line1.text()
         numinput2 = self.line2.text()
         numinput3 = self.line4.text()
         numinput4 = self.line5.text()
   
-
+        
         m = open("matrix.txt","r")
         m2 = m.read()
         m3 = m2.replace(","," ")
         matrix = np.asarray(m3)
-        
-        ncols = 0
-        ncols = ncols +1
-        #while loop
-        total_array[0,ncols+1] = numinput0
-        total_array[1,ncols+1] = numinput1  
-        total_array[2,ncols+1] = numinput2
-        total_array[3,ncols+1] = matrix
-        total_array[4,ncols+1] = numinput3
-        total_array[5,ncols+1] = numinput4
-        print total_array     
-        return total_array
-            
 
-        m_array2 = np.array([[numinput0],[numinput1],[numinput2],[matrix],[numinput3],[numinput4]])
-      
-        #num_rows, num_cols = m_array2.shape
-        ## wrong because num_cols of m_array2 is always = 1
-        #print num_cols
+        nonzeros = np.count_nonzero(self.create_arrays()[:,1])
+        print nonzeros
+
+        if nonzeros == 0:
+            global ncols
+            ncols = 0
+            self.create_arrays()[0,ncols+1] = numinput0
+            self.create_arrays()[1,ncols+1] = numinput1  
+            self.create_arrays()[2,ncols+1] = numinput2
+            self.create_arrays()[3,ncols+1] = matrix
+            self.create_arrays()[4,ncols+1] = numinput3
+            self.create_arrays()[5,ncols+1] = numinput4
+                
+            print "working"
+            ncols = ncols +1
+     
+            return self.create_arrays()
+ 
+        else:
+            print "ok"
         
-        #if num_cols == 1:
-         #   narray = np.hstack((narray0,m_array2))
-          #  narray=np.hstack((narray,m_array2))
-           # narray = narray[:,1:]
-            #print "empty"
-       # else:
-        #    narray=np.hstack((narray,m_array2)) 
-         #   print "working"
-        	
-        #return narray
-       
-        
+            self.create_arrays()[0,ncols+1] = numinput0
+            self.create_arrays()[1,ncols+1] = numinput1  
+            self.create_arrays()[2,ncols+1] = numinput2
+            self.create_arrays()[3,ncols+1] = matrix
+            self.create_arrays()[4,ncols+1] = numinput3
+            self.create_arrays()[5,ncols+1] = numinput4
+            ncols = ncols +1
+            
+            
+            return self.create_arrays()  
         
     def save_files(self):
-        #num_rows, num_cols = self.store_input().shape
 
         files_names=[]
         for i in range(0,10):
             files_names.append("input"+str(i)+".txt")
-            
-            with open(files_names[0],"w") as f:
-                for n in range(0,6):
-                    f.write("# " + self.store_input()[n,0])
-                    f.write("\n")
-                    f.write(self.store_input()[n,1])
-                    f.write("\n\n")
+
+            with open(files_names[i], "w") as f:
+                for cols in range(0,ncols+1):          
+                    for n in range(0,6):    
+                        f.write("# " + self.store_input()[n,0])
+                        f.write("\n")
+                        f.write(self.store_input()[n,cols])
+                        f.write("\n\n")
+          
         
 
         
