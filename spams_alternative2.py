c57e022addf3bd14ea5be238c2fd5cba135409b0
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-03-06 15:51:07 +0000

    saves input to file
    creates array where each column corresponds to different input. Saves data from each column to a different file

--- spams_alternative2.py
+++ spams_alternative2.py
@@ -3,31 +3,35 @@
 from PyQt4 import QtCore, QtGui
 import sys, csv
 import numpy as np
-from teste import save_file
+
 
 class Dialog(QtGui.QDialog):
 
     def __init__(self):
         super(Dialog, self).__init__()
+        
+        mainLayout = QtGui.QVBoxLayout()
 
         self.createHorizontalGroupBox()
         self.createGridGroupBox()
+        mainLayout.addWidget(self.horizontalGroupBox)
+        mainLayout.addWidget(self.gridGroupBox)
       
 
         buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel)
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
-        create_docBtn = QtGui.QPushButton("Create .txt")
-        create_docBtn.clicked.connect(self.create_txt2)
-        create_docBtn.clicked.connect(self.save_file2)
+
         
-      
-     
-        mainLayout = QtGui.QVBoxLayout()
+        self.input_btn = QtGui.QPushButton("Save input")
+        self.input_btn.clicked.connect(self.store_input)
+
+        self.file_input = QtGui.QPushButton("Save doc")
+        self.file_input.clicked.connect(self.save_files)
         
-        mainLayout.addWidget(self.horizontalGroupBox)
-        mainLayout.addWidget(self.gridGroupBox)
-        mainLayout.addWidget(create_docBtn)       
+        
+        mainLayout.addWidget(self.input_btn)
+        mainLayout.addWidget(self.file_input)
         mainLayout.addWidget(buttonBox)
        
    
@@ -35,30 +39,6 @@ class Dialog(QtGui.QDialog):
 
         self.setLayout(mainLayout)
         self.setWindowTitle("SPAms alternative")
-
-
-       
-
-    def save_file2(self):
-
-        numinput0 = self.line0.text()
-        numinput1 = self.line1.text()
-        numinput2 = self.line2.text()
-        numinput3 = self.line4.text()
-        numinput4 = self.line5.text()
-        m_array = np.zeros((3,3))
-        m_array[0,0] = numinput0
-        m_array[1,0] = numinput1
-        m_array[2,0] = numinput2
-        matr_array = np.genfromtxt("matrix.txt",delimiter = ",")
-        m_array2 = np.zeros((2,3))
-        m_array2[0,0] = numinput3
-        m_array2[1,0] = numinput4
-        #final_array = np.vstack((m_array, matr_array, m_array2))
-        final_array = np.vstack((m_array, m_array2))
-        save_file(final_array)
-  
-        
 
     
     def new_matrix2(checked):
@@ -68,56 +48,14 @@ class Dialog(QtGui.QDialog):
             path_open = open(path)
             path_open2 = path_open.read()
             path_open3 = path_open2.replace(","," ")
-
+      
             print "working"
-
-
-    def create_txt2(self):
-        sections = ("Number of loci", "Sampling Vector", "Initial deme sizes", "Initial migration matrix")
-        sections2 =( "Time of change", "Deme sizes")
-        ninput = ("numinput0", "numinput1", "numinput2", "numinput3", "numinput4")
-
-        numinput0 = self.line0.text()
-        numinput1 = self.line1.text()
-        numinput2 = self.line2.text()
-        numinput3 = self.line4.text()
-        numinput4 = self.line5.text()
-
-        
-        
-        
-        filepath = QtGui.QFileDialog.getSaveFileName(self, 'Save file',"", '*.txt')
-        fileHandle = open(filepath, "w")
-        
-        fileHandle.write("# "+sections[0]+"\n")
-        fileHandle.write(numinput0+"\n\n")
-        fileHandle.write("# "+sections[1]+"\n")
-        fileHandle.write(numinput1+"\n\n")
-        fileHandle.write("# "+sections[2]+"\n")
-        fileHandle.write(numinput2+"\n\n")
-        fileHandle.write("# "+sections[3]+"\n")
-        for line in path_open3:
-            fileHandle.write(line)
-        fileHandle.write("\n")
-        fileHandle.write("# "+sections2[0]+"\n")
-        fileHandle.write(numinput3+"\n\n")
-        fileHandle.write("# "+sections2[1]+"\n")
-        fileHandle.write(numinput4+"\n\n")
-        
-        
-        fileHandle.close()
-       
-
         
     def new_window(self):
         self.wind.show()
     def new_gwindow(self):
         self.gwindow.show()
 
-    def add_file(self):
-        path = QtGui.QFileDialog.getOpenFileName(
-                self, 'Upload File', '', '*.txt')
-    
         
 #creates the buttons for the models and opens a window when you click them
     def createHorizontalGroupBox(self):
@@ -156,6 +94,7 @@ class Dialog(QtGui.QDialog):
         layout.addWidget(self.line4, 5,1)
         self.line5 = QtGui.QLineEdit()        
         layout.addWidget(self.line5, 6,1)
+        
 
 
         self.button_matr = QtGui.QPushButton("Create matrix")
@@ -170,11 +109,90 @@ class Dialog(QtGui.QDialog):
                 
         
         self.gridGroupBox.setLayout(layout)
+
+    
+
+    def run_once(func):
+        def decorated(*args, **kwargs):
+            try:
+                return decorated._once_result
+            except AttributeError:
+                decorated._once_result = func(*args, **kwargs)
+                return decorated._once_result
+        return decorated
+
+    @run_once
+    def create_arrays(self):
+        init_array = np.zeros((6,10),dtype=object)
+        sect_array = np.array([["Number of loci"],["Sampling vector"],["Initial deme sizes"],["Initial migration matrix"],["Time of change"],["Deme sizes"]])
+        sect_array = np.vstack(sect_array)
+        total_array = np.hstack((sect_array,init_array))
+        #print total_array
+        return total_array
+    
+
+    def store_input(self):
+        numinput0 = self.line0.text()
+        numinput1 = self.line1.text()
+        numinput2 = self.line2.text()
+        numinput3 = self.line4.text()
+        numinput4 = self.line5.text()
+  
+        global nonzeros
+        nonzeros = np.count_nonzero(self.create_arrays()[:,1])
+        print nonzeros
+
+        if nonzeros == 0:
+            global ncols
+            ncols = 0
+            self.create_arrays()[0,ncols+1] = numinput0
+            self.create_arrays()[1,ncols+1] = numinput1  
+            self.create_arrays()[2,ncols+1] = numinput2
+            self.create_arrays()[3,ncols+1] = path_open3
+            self.create_arrays()[4,ncols+1] = numinput3
+            self.create_arrays()[5,ncols+1] = numinput4
+                
+            print "working"
+            ncols = ncols +1
+            global final_array
+            final_array = self.create_arrays()
+            #print final_array
+            return final_array
+ 
+        else:
+            print "ok"
+        
+            final_array[0,ncols+1] = numinput0
+            final_array[1,ncols+1] = numinput1  
+            final_array[2,ncols+1] = numinput2
+            final_array[3,ncols+1] = path_open3
+            final_array[4,ncols+1] = numinput3
+            final_array[5,ncols+1] = numinput4
+            ncols = ncols +1
+            
+            #print final_array
+            return final_array 
+        
+    def save_files(self):
+
+        files_names=[]
+        for i in range(0, ncols):
+            files_names.append("input"+str(i)+".txt")
+
+            with open(files_names[i], "w") as f:
+                #for cols in range(1,ncols+1):
+                for row in range(0,6):
+                    f.write("# " + final_array[row,0])
+                    f.write("\n")
+                    f.write(final_array[row,i+1])
+                    f.write("\n\n")
+
       
 class Second_W(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(Second_W, self).__init__(parent)
         self.setGeometry(300, 300, 350, 300)
+
         
 
 class grid_window(QtGui.QMainWindow):
