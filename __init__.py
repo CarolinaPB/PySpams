1a7c5ec5ea03eab641a8cbffa3b1d15916d7237f
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-03-13 18:12:38 +0000

    Files for web page
    Opens a page in the browser with a simple form where the user can input their data.
    The data can be saved in a file.
    The form can be reset by clicking a button

---
+++ __init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import numpy as np
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("main.html")
+    
+
+@app.route('/receive_input', methods=['POST'])
+def handle_input():
+    sections = ("numloci","sampvector", "inideme","inimatr", "timechange","demesizes", "filename")
+    sections_names = ("Number of loci", "Sampling Vector", "Initial deme sizes","Initial migration matrix", "Time of change", "Deme sizes")        
+    data =[]
+    if request.method =="POST":
+        for i in range (0,7):
+            data.append(request.form[sections[i]])
+        print data
+    
+    
+    with open(request.form[sections[6]],"w") as f:
+        for i in range (0,6):
+            f.write ("# " + sections_names[i])
+            f.write ("\n")
+            f.write (request.form[sections[i]])
+            f.write ("\n\n")
+    return "Your data has been saved in a file"
+       
+
+
+
+if __name__ == "__main__":
+    app.run()
