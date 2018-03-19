3cd59e3353ead282a52d4f08efe5ca0fe823b720
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-03-19 15:17:53 +0000

    Saves file with 1 matrix

--- __init__.py
+++ __init__.py
@@ -10,8 +10,6 @@ sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def homepage():
     return render_template("main.html")
@@ -21,50 +19,47 @@ def homepage():
 UPLOAD_FOLDER = 'uploads/'
 app.config["UPLOADFOLDER"] = UPLOAD_FOLDER
 
-@app.route("/", methods = ["GET", "POST"])
-def upload():
-    if request.method =="POST":
-        file = request.files["file"]
-        if file:
-            global filename
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-    return "matrix saved"
-
-    
 
 @app.route('/receive_input', methods=['POST',"GET"])
 def handle_input():
-    sections = ("numloci","sampvector", "inideme","filename", "timechange","demesizes", "filename2")
-    sections_array =np.array([["Number of loci"],["Sampling vector"],["Initial deme sizes"],["Initial migration matrix"],["Time of change"],["Deme sizes"], ["File name"]]) 
-    
+    if request.method =="POST":
+        file = request.files["file"]
+        if file:
+            #global filename
+            filename = secure_filename(file.filename)
+            print filename
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+
+    sections = ("numloci","sampvector", "inideme","inideme", "timechange","demesizes", "filename2")
+    sections_array =np.array([["Number of loci"],["Sampling vector"],["Initial deme sizes"],["Initial migration matrix"],["Time of change"],["Deme sizes"], ["File name"]])
+
     init_array = np.zeros((7,1), dtype=object)
     total_array = np.hstack((sections_array,init_array))
-    global filename
-    path = ("/home/carolina/flask_app/flask_app/uploads/" + str(filename))
-    matr = open(path,"r")
-    matr=matr.read()
-    print matr
-                
-    for i in range(0,7):
-        
-        total_array[i,1] = request.form[sections[i]]
-        
-    #not saving properly
-        with open(request.form[sections[6]],"w") as f:
-            for i in range(0,6):
-                f.write("# " + total_array[i,0])
-                f.write("\n")
-                f.write("# " + str(total_array[i,1]))
-                f.write("\n\n")
-                
-                    
-                
-    print total_array
-  
+
+
+    for i in range (0,3):
+        total_array[i,1]=request.form[sections[i]]
+    for i in range (4,6):
+        total_array[i,1]=request.form[sections[i]]
+
+    file_path = ("/home/carolina/flask_app/flask_app/uploads/" + str(filename))
+    filehandle = open(file_path,"r")
+    filehandle=filehandle.read()
+    filehandle=filehandle.replace(","," ")
+
+    total_array[3,1] = filehandle
+
+    with open(request.form[sections[6]],"w") as f:
+        for i in range(0,6):
+            f.write("# " + total_array[i,0])
+            f.write("\n")
+            f.write("# " + str(total_array[i,1])+"\n")
+            f.write("\n")
+
+    os.remove(file_path)
     return render_template("main.html")
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
