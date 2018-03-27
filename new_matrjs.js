d081825ad6847c1955f42038ac8e7b78b348a069
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-03-27 17:27:09 +0100

    Can add as many matrix fields as the user wants
    The ids from each of the new fields are not correct for now.
    The button to add a matrix file will be moved to its place
    The button to add more fields will be moved

--- new_matrjs.js
+++ new_matrjs.js
@@ -1,40 +1,50 @@
+section_names=["file", "timechange", "demesizes"]
+sections_label=["Initial migration matrix","Time of change","Deme sizes"]
+var nmatr=0;
 $(document).ready(function(){
-  $(".hidden_fields1").hide();
-  $(".btn_repeat").click(function(){
-    $(".hidden_fields1").show();
+  $("#btn_repeat").click(function(){
+
+    function nmatr_counter(){
+      nmatr=nmatr+1;
+      alert(nmatr);
+    }
+    for (i=0;i<section_names.length; i++) {
+
+      //create labels
+      var makelabel = document.createElement("label");
+
+      //create spans
+      var spans = document.createElement("span");
+      var sect_labels = document.createTextNode(sections_label[i]);
+      spans.appendChild(sect_labels);
+
+      //create input elements type="text"
+      var makeinput = document.createElement("input");
+      makeinput.className="fields_to_repeat";
+      makeinput.setAttribute("class", "fields_to_repeat");
+      makeinput.setAttribute("id", section_names[i]+i);
+      makeinput.setAttribute("name", section_names[i]+i)
+      makeinput.setAttribute("type", "text");
+
+      //create input elements type="file"
+      var makeinput_file = document.createElement("input");
+      makeinput_file.className="fields_to_repeat";
+      makeinput_file.setAttribute("class", "fields_to_repeat")
+      makeinput_file.setAttribute("id", section_names[0]+1);
+      makeinput_file.setAttribute("name", section_names[i]+i)
+      makeinput_file.setAttribute("type", "file")
+
+      //create line break
+      var br = document.createElement("br")
+
+      //append to parent div
+      makelabel.appendChild(spans);
+      makelabel.appendChild(makeinput);
+
+      $("#fields_to_repeat").append(makelabel);
+      $(makelabel).after(br);
+
+    }
+    $("#fields_to_repeat").append(makeinput_file);
   });
-
-
 });
-
-
-
-// Goal:
-//nmatr=0
-//#if clicked:
-
-//nmatr=nmatr+1
-//for i in range(0,nmatr+1):
- //   file="file"+str(i)
-   // timechange="timechange"+str(i)
-    //demesizes="demesizes"+str(i)
-
-//section_names=[file, timechange, demesizes]
-//print section_names
-
-  //    <div class="hidden_fields">
-    //    <br><br>
-      //  <label>
-        //    <span >Initial migration matrix</span><br>
-          //  <input name=section_names[0] type="file"/> <br>
-        //</label><br>
-        //<label>
-          //  <span >Time of change</span><br>
-           // <input name=section_names[1] type="text"/> <br>
-       // </label><br>
-        //<label>
-         //   <span >Deme sizes</span><br>
-          //  <input name=section_names[2] type="text"/> <br>
-        //</label><br>
-        //<button type="button"class="btn_repeat">Add matrix</button>
-      //</div>
