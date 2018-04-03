7e6182196fc44a395e391a39bdfdd2d503fdfc3b
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-04-03 18:04:47 +0100

    Add how many matrices input as you want
    Each new field has a unique Id
    Currently not saving
    Need to work on the new fields' styling to match the rest

--- new_matrjs.js
+++ new_matrjs.js
@@ -1,57 +1,74 @@
-section_names=["file", "timechange", "demesizes"]
-sections_label=["Initial migration matrix","Time of change","Deme sizes"]
+section_names=["file", "timechange", "demesizes"];
+sections_label=["Initial migration matrix","Time of change","Deme sizes"];
+
+
 
 $(document).ready(function(){
+  var button = document.getElementById('btn_repeat'),
+     count=0;
+  button.onclick = function(){
+    count += 1;
+  };
   $("#btn_repeat").click(function(){
+    console.log("count is "+count)
 
-    //function clickCounter() {
-      //  if(typeof(Storage) !== "undefined") {
-        //    if (localStorage.clickcount) {
-          //      localStorage.clickcount = Number(localStorage.clickcount)+1;
-           // } else {
-             //   localStorage.clickcount = 1;
-            //}
-        //}
-    //}
-    //console.log(localStorage.clickcount);
+    //for (var i in count){
+      //console.log("count  "+i);
+   // }
 
-    for (i=0;i<section_names.length; i++) {
+    //create labels
+    var makelabel = document.createElement("label");
 
-      //create labels
-      var makelabel = document.createElement("label");
+    //create spans
+    var spans0 = document.createElement("span");
+    var sect_labels0 = document.createTextNode(sections_label[0]);
+    spans0.appendChild(sect_labels0);
+    var spans1 = document.createElement("span");
+    var sect_labels1 = document.createTextNode(sections_label[1]);
+    spans1.appendChild(sect_labels1);
+    var spans2 = document.createElement("span");
+    var sect_labels2 = document.createTextNode(sections_label[2]);
+    spans2.appendChild(sect_labels2);
 
-      //create spans
-      var spans = document.createElement("span");
-      var sect_labels = document.createTextNode(sections_label[i]);
-      spans.appendChild(sect_labels);
+    //create input elements type="file"
+    var makeinput_file = document.createElement("input");
+    makeinput_file.className="fields_to_repeat";
+    makeinput_file.setAttribute("class", "fields_to_repeat")
+    makeinput_file.setAttribute("id", section_names[0])+count;
+    makeinput_file.setAttribute("name", section_names[0])+count;
+    makeinput_file.setAttribute("type", "file");
 
-      //create input elements type="text"
-      var makeinput = document.createElement("input");
-      makeinput.className="fields_to_repeat";
-      makeinput.setAttribute("class", "fields_to_repeat");
-      makeinput.setAttribute("id", section_names[i]+i);
-      makeinput.setAttribute("name", section_names[i]+i)
-      makeinput.setAttribute("type", "text");
+    //create input elements type="text"
+    var makeinput1 = document.createElement("input");
+    makeinput1.className="fields_to_repeat";
+    makeinput1.setAttribute("class", "fields_to_repeat");
+    makeinput1.setAttribute("id", section_names[1])+count;
+    makeinput1.setAttribute("name", section_names[1]+count);
+    makeinput1.setAttribute("type", "text");
+    makeinput1.setAttribute("placeholder", section_names[1]+count);
 
-      //create input elements type="file"
-      var makeinput_file = document.createElement("input");
-      makeinput_file.className="fields_to_repeat";
-      makeinput_file.setAttribute("class", "fields_to_repeat")
-      makeinput_file.setAttribute("id", section_names[0]+1);
-      makeinput_file.setAttribute("name", section_names[i]+i)
-      makeinput_file.setAttribute("type", "file")
+    var makeinput2 = document.createElement("input");
+    makeinput2.className="fields_to_repeat";
+    makeinput2.setAttribute("class", "fields_to_repeat");
+    makeinput2.setAttribute("id", section_names[2]+count);
+    makeinput2.setAttribute("name", section_names[2]+count);
+    makeinput2.setAttribute("type", "text");
+    makeinput2.setAttribute("placeholder", section_names[2]+count);
 
-      //create line break
-      var br = document.createElement("br")
+    //create line break
+    var br = document.createElement("br");
 
-      //append to parent div
-      makelabel.appendChild(spans);
-      makelabel.appendChild(makeinput);
+    //append to parent div
+    makelabel.appendChild(spans0);
+    makelabel.appendChild(makeinput_file);
+    makelabel.appendChild(spans1);
+    makelabel.appendChild(makeinput1);
+    makelabel.appendChild(spans2);
+    makelabel.appendChild(makeinput2);
 
-      $("#fields_to_repeat").append(makelabel);
-      $(makelabel).after(br);
+    $("#fields_to_repeat").append(makelabel);
+    //$(makelabel).after(br);
 
-    }
-    $("#fields_to_repeat").append(makeinput_file);
+    //$("#fields_to_repeat").append(makeinput_file);
   });
 });
