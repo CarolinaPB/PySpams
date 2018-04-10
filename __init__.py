0ca7bae92db02dae945fefffd673294c823e04f7
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-04-10 16:05:11 +0100

    Creates array with unique names/id for each field
    Creates an array ("sections") with the name/id from each field. For each new matr it adds a new group of unique names to the array. Each field has an unique name.

--- __init__.py
+++ __init__.py
@@ -3,10 +3,6 @@ import numpy as np
 from werkzeug import secure_filename
 import os, sys, tempfile
 
-#from flask_wtf import FlaskForm
-#from wtforms import StringField, SubmitField
-
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -22,7 +18,7 @@ UPLOAD_FOLDER = 'uploads/'
 app.config["UPLOADFOLDER"] = UPLOAD_FOLDER
 
 global count
-count = 0
+count = 1
 
 
 def counter ():
@@ -35,48 +31,29 @@ def handle_input():
     if request.form["button"] == "Add matrix":
     #creates a counter for the "add matrix" button
         counter()
+        print "count is " + str(count)
         return (str(count), 204)
     elif request.form["button"] == "Save to file":
-    #will save info to a file
 
-######## not working properly
-        sections = np.array([["filename"],["numloci0"], ["sampvector0"], ["inideme0"]])
-        part_sections=np.array([["file"], ["timechange"],["demesizes"]])
-        sections = np.hstack(sections)
-        part_sections = np.hstack(part_sections)
+######## Creates an array ("sections") with the names/id from each field. For each new matr it adds a new group of unique names for the new fields.
 
-        if count == 0:
-            part_sections[0] = part_sections[0]+str(0)
-            part_sections[1]= part_sections[1] + str(0)
-            part_sections[2]= part_sections[2] + str(0)
-
-            sections = np.hstack((sections, part_sections))
-
-        else:
-            part_sections = np.tile(part_sections,(count+1,1))
-            part_sections = np.hstack(part_sections)
-
-            sections = np.hstack((sections,part_sections))
-
-
-        print sections
-
-
-        for i in range(4,len(sections),3):
-            for n in range(0,count+1):
-                sections[i] = sections[i]+str(count)
-                sections[i+1] = sections[i+1]+str(count)
-                sections[i+1] = sections[i+1]+str(count)
-
-            print sections[i]
-            print sections[i+1]
-            print sections[i+2]
+        if count == 1:
+            sections = np.array([["filename"],["numloci0"], ["sampvector0"], ["inideme0"],["file0"], ["timechange0"],["demesizes0"]],dtype=object)
+        elif count >= 2:
+            sections = np.array([["filename"],["numloci0"], ["sampvector0"], ["inideme0"],["file0"], ["timechange0"],["demesizes0"]],dtype=object)
+            for n in range(1,count):
+                part_sections=np.array([["file"], ["timechange"],["demesizes"]],dtype=object)
+                part_sections[0] = part_sections[0]+str(n)
+                part_sections[1] = part_sections[1]+str(n)
+                part_sections[2] = part_sections[2]+str(n)
+                sections = np.append([sections], [part_sections])
+                sections = np.vstack(sections)
 
 ########
 
         sections_array = np.array([["File name"],["Number of loci"],["Sampling vector"],["Initial deme sizes"]])
+        part_sections_arr=np.array([["Initial migration matrix"], ["Time of change"], ["Deme sizes"]])
 
-        part_sections_arr=("Initial migration matrix", "Time of change", "Deme sizes")
         part_sections_arr = np.vstack(part_sections_arr)
 
         if count == 0:
@@ -85,13 +62,13 @@ def handle_input():
             part_sections_arr = np.tile(part_sections_arr,(count+1,1))
 
         sections_array2=np.vstack((sections_array,part_sections_arr))
-        print "\n\n\n"
-        print sections_array2
+        #print "\n\n\n"
+        #print sections_array2
 
         init_array = np.zeros((6+3*count+1), dtype=object)
         init_array = np.vstack(init_array)
         total_array = np.hstack((sections_array2, init_array))
-        print "\n\n\n"
+        #print "\n\n\n"
 
         #saves the first matrix to the computer
         if request.method =="POST":
@@ -101,8 +78,8 @@ def handle_input():
                 #print filename
                 file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-        for i in range(4):
-            total_array[i,1] = request.form[sections[i]]
+        #for i in range(4):
+         #   total_array[i,1] = request.form[sections[i]]
 
         total_array[5,1] = request.form["timechange0"]
         total_array[6,1] = request.form["demesizes0"]
@@ -115,7 +92,11 @@ def handle_input():
         filehandle=filehandle.replace(","," ")
 
         total_array[4,1] = filehandle
-        print total_array
+        #print total_array
+
+        #to reset the count variable to 1
+        global count
+        count = 1
 
         os.remove(file_path)
 
