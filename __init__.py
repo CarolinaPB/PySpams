7f279ee1ff8d947052202cc6987e401bf2c0aaee
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2018-04-20 17:42:13 +0100

    checkbox to use or not the input
    If not checked, the input is hidden and will not be saved in the doc.
    If checked, the input will be saved in the doc.
    Can be toggled between checked and unchecked

--- __init__.py
+++ __init__.py
@@ -30,19 +30,13 @@ def counter ():
     return ('', 204)
 
 
-
 @app.route("/", methods=["POST"])
 def handle_input():
-
-    #if request.form["group_id"] == "hidden":
-     #   print "maybe"
-      #  return ("", 204)
 
     if request.form["button"] == "Add matrix":
     #creates a counter for the "add matrix" button
         counter()
         print "count is " + str(count)
-
         return (str(count), 204)
 
     # resets "count" if "Reset form" is clicked
@@ -51,6 +45,16 @@ def handle_input():
         print "reset" +str(counter)
         return (str(count),204)
     elif request.form["button"] == "Save to file":
+######## For each unchecked checkbox, it does count-1 because there will be count-1 new inputs (matrices) used
+        chk_list=[]
+        for i in range(1,count):
+            chk_list.append("chk_remove"+str(i))
+        for name in range(0,len(chk_list)):
+            if request.form[chk_list[name]] == "Remove":
+                print chk_list[name]
+            elif request.form[chk_list[name]] == "unchecked":
+                count = count-1
+                print "This input will not count"
 
 ######## Creates a list ("sections") with the names/id from each field. For each new matr it adds a new group of unique names for the new fields.
 ######## Creates a list with the section names which will be in the final doc
@@ -59,7 +63,7 @@ def handle_input():
             sections = ["filename", "numloci0", "sampvector0", "inideme0", "file0", "timechange0", "demesizes0"]
             sections_list = ["File name", "Number of loci", "Sampling Vector", "Initial deme sizes", "Initial migration matrix", "Time of change", "Deme sizes"]
 
-        if count >= 2:
+        elif count >= 2:
             sections = ["filename", "numloci0", "sampvector0", "inideme0", "file0", "timechange0", "demesizes0"]
             sections_list = ["File name", "Number of loci", "Sampling Vector", "Initial deme sizes", "Initial migration matrix", "Time of change", "Deme sizes"]
             part_sections_list=[]
--- new_matrjs_alt.js
+++ new_matrjs_alt.js
@@ -1,14 +1,36 @@
 section_names=["file", "timechange", "demesizes"];
 
 var count = 0;
+var div_ids = [];
+
+
+function Checkbox(e){
+
+  for (var id in div_ids){
+    classname_div = document.getElementById(div_ids[id]).className;
+    classname_chk = e.className;
+
+    if (classname_div == classname_chk){
+      if (e.checked == false){
+        //alert("unchecked "+"div " + classname_div);
+        $("#"+div_ids[id]).hide();
+      } else {
+        //alert("checked "+"chk " +classname_chk);
+        $("#"+div_ids[id]).show();
+      };
+    };
+  };
+};
+
 
 $(document).ready(function(){
 
   $("#btn_repeat").on("click", function(){
     count++;
+    div_ids.push("repeated_fields"+count);
 
     var content =
-    '<div id="repeated_fields' + count + '">'+
+    '<div class="myclass' + count + '"  id="repeated_fields' + count + '">'+
 
     '<label>' + 'Initial migration matrix' + '</label>' + "<br>"+
     '<input type="file" name= '+ section_names[0] +'' + count + ' id='+ section_names[0] +'' + count + ' >' + '<br>'+"<br>"+'<br>'+
@@ -19,21 +41,14 @@ $(document).ready(function(){
     '<label>' + 'Deme sizes' + '</label>' + "<br>"+
     '<input type="text" name= '+ section_names[2] +'' + count + ' id='+ section_names[2] +'' + count + ' >' + '<br>'+"<br>" +
     '</div>'+
-    '<input type="checkbox" id="chk_remove" name="chk_remove" value="Remove" checked>'+'Use this input'+
-    '<label>'+ '**name of the chosen file**' +'<br>'  ;
+
+    '<input type="checkbox" id="chk_remove' + count + '" name="chk_remove' + count + '" value="Remove" checked onclick="Checkbox(this)"class="myclass' + count + '">'+
+    '<label for"chk_remove">   Use</label>'+
+    '<input type="hidden" name="chk_remove' + count + '" value="unchecked">'+
+    '<br>'+'<br>' +'<br>' ;
 
     $("#fields_to_repeat").append(content);
 
-
-    $("#chk_remove").change(function(){
-      if (this.checked){
-        $("#repeated_fields"+count).show();
-      } else {
-        $("#repeated_fields"+count).hide();
-      }
-    });
-
   });
-
 
 });
